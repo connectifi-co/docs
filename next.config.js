6c4fa1e9f91f4b430b5829710d60cb667f34d160
ruby childs <ruby.childs1@gmail.com>
2024-11-12 15:06:42 -0500

    working on github deployment

--- next.config.js
+++ next.config.js
@@ -3,9 +3,16 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx'
 })
 
+const isProduction = process.env.NODE_ENV === "production";
+const assetPrefix = isProduction ? "/out" : "";
+
 module.exports = {
   ...withNextra(),
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
+  assetPrefix,
+  basePath: assetPrefix,
+  output: "out",
 };
